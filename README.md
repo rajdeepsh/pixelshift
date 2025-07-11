125a127767a858df556a8838a1a6cdc9f124106f
rajdeepsh <rajdeepsh@icloud.com>
2025-07-11 16:32:53 +0800

    add crop [untested]

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Common image transformations
 
 ## Usage
 
+> [!TIP]
+> You can find more information with `uv run main.py --help`.
+
 ### Replace Pixels
 Replace all occurances of the first RGB value with the second RGB value in the source image. The resulting image will be saved to the destination path.
 ```bash
@@ -15,5 +18,13 @@ uv run main.py --src <source_image_path> --dst <destination_image_path> --replac
 uv run main.py --src ~/img.png --dst ~/img2.png --replace 255 255 255 0 0 0
 ```
 
-> [!TIP]
-> You can find more information with `uv run main.py --help`.
+> [!WARNING]
+> Untested 
+### Crop Image
+Crop image according to top-left and bottom-right XY coordinates.
+```bash
+# Format
+uv run main.py --src <source_image_path> --dst <destination_image_path> --crop <top_left_x> <top_left_y> <bottom_right_x> <bottom_right_y>
+# Example which crops an image 
+uv run main.py --src ~/img.png --dst ~/img2.png --crop x y x y
+```
--- main.py
+++ main.py
@@ -17,7 +17,10 @@ class Args:
     """Destination image path"""
 
     replace: tuple[tuple[int, int, int], tuple[int, int, int]] | None = None
-    """Replace all occurances of the first RGB value with the second RGB value"""
+    """Replace all occurances of the first RGB value with the second RGB value. Provide RGB values in the form of <first_red> <first_green> <first_blue> <second_red> <second_green> <second_blue>."""
+
+    crop: tuple[tuple[int, int], tuple[int, int]] | None = None
+    """Crop image according to top-left and bottom-right XY coordinates. Provide coordinates in the form of <top_left_x> <top_left_y> <bottom_right_x> <bottom_right_y>."""
 
 
 def replace(img, src_rgb, dst_rgb):
@@ -27,11 +30,20 @@ def replace(img, src_rgb, dst_rgb):
     img[mask] = dst_bgr
 
 
+def crop(img, top_left_xy, btm_right_xy):
+    x1, y1 = top_left_xy
+    x2, y2 = btm_right_xy
+    return img[y1:y2, x1:x2]
+
+
 def main(args):
     img = cv2.imread(args.src, cv2.IMREAD_COLOR)
 
     if args.replace is not None:
         replace(img, *args.replace)
+
+    if args.crop is not None:
+        crop(img, *args.crop)
 
     cv2.imwrite(args.dst, img)
 
